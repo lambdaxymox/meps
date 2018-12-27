aa140dbb7e9acb0a748ccf9e89565e2fc54e0611
LambdaXymox <lambda.xymox@gmail.com>
2018-12-27 00:33:53 -0600

    Implement main to run meps.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::f32;
 use std::f64;
 use std::env;
+use std::process;
 
 
 fn machine_eps_f32(value: f32) -> (f32, f32) {
@@ -43,6 +44,29 @@ fn machine_eps_f64(value: f64) -> (f64, f64) {
     (f64::abs(value - value_minus_one), f64::abs(value_plus_one - value))
 }
 
+fn usage() -> String {
+    format!("USAGE: meps <floating point number>")
+}
+
 fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    if args.len() != 2 {
+        println!("{}", usage());
+        process::exit(1);
+    }
     
+    let num_str =  &args[1];
+    let maybe_num_f32 = str::parse::<f32>(&num_str); 
+    let maybe_num_f64 = str::parse::<f64>(&num_str);
+
+    if maybe_num_f32.is_err() || maybe_num_f64.is_err() {
+        eprintln!("The value {} does not represent a valid floating point number.", num_str);
+        process::exit(1);
+    }
+
+    let num_f32 = maybe_num_f32.unwrap();
+    let num_f64 = maybe_num_f64.unwrap();
+
+    println!("{:?}", machine_eps_f32(num_f32));
+    println!("{:?}", machine_eps_f64(num_f64));
 }
